2101a994cd7c8e9e9368c13406f84d1122156475
szTheory <szTheory@users.noreply.github.com>
2026-06-14 14:57:27 -0400

    feat(110-01): add Vanniktech Maven Central publish config to Android core
    - Add com.vanniktech.maven.publish 0.31.0 plugin to build.gradle.kts plugins block
    - Add version = "0.1.0" // x-release-please-version annotation for release-please generic updater
    - Add mavenPublishing block: publishToMavenCentral(CENTRAL_PORTAL, automaticRelease=false)
    - Add signAllPublications() for GPG signing
    - Add coordinates("io.github.sztheory", "crosswake-shell-core-android", ...) (Maven groupId distinct from Android namespace)
    - Add complete POM with all six required fields: name, description, url, licenses, developers, scm
    - USER_MANAGED mode (automaticRelease=false) is load-bearing: stops at VALIDATED, never auto-publishes (D-07)

--- packages/crosswake-shell-core-android/build.gradle.kts
+++ packages/crosswake-shell-core-android/build.gradle.kts
@@ -2,7 +2,10 @@ plugins {
     id("com.android.library")
     id("org.jetbrains.kotlin.android")
     id("kotlinx-serialization")
+    id("com.vanniktech.maven.publish") version "0.31.0"
 }
+
+version = "0.1.0" // x-release-please-version
 
 android {
     namespace = "dev.crosswake.shell.core"
@@ -32,4 +35,40 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
     // Note: Android bridge may use WebKit/WebView and we might need androidx.webkit
     implementation("androidx.webkit:webkit:1.10.0")
+}
+
+mavenPublishing {
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL, automaticRelease = false)
+    signAllPublications()
+
+    coordinates("io.github.sztheory", "crosswake-shell-core-android", version.toString())
+
+    pom {
+        name.set("Crosswake Shell Core Android")
+        description.set("Android native shell core for the Crosswake Phoenix library.")
+        inceptionYear.set("2024")
+        url.set("https://github.com/szTheory/crosswake")
+
+        licenses {
+            license {
+                name.set("MIT License")
+                url.set("https://opensource.org/licenses/MIT")
+                distribution.set("https://opensource.org/licenses/MIT")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("szTheory")
+                name.set("szTheory")
+                url.set("https://github.com/szTheory/")
+            }
+        }
+
+        scm {
+            url.set("https://github.com/szTheory/crosswake/")
+            connection.set("scm:git:git://github.com/szTheory/crosswake.git")
+            developerConnection.set("scm:git:ssh://git@github.com/szTheory/crosswake.git")
+        }
+    }
 }
