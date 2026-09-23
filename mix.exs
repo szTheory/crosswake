a8d86b29ee5731332ceac9abc176ecdc8262e7e1
szTheory <szTheory@users.noreply.github.com>
2026-09-22 20:13:12 -0400

    chore: release main

--- mix.exs
+++ mix.exs
@@ -2,7 +2,7 @@ defmodule Crosswake.MixProject do
   use Mix.Project
 
   # x-release-please-version
-  @version "0.2.2"
+  @version "0.2.3"
   @source_url "https://github.com/szTheory/crosswake"
 
   def project do
